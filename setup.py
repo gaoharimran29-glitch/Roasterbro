e39a553bce52577647b3aabbe91fc0d620a7c9b6
Gaohar Imran <gaoharimran29@gmail.com>
2026-08-15 21:20:27 +0530

    Mirrored requirements.txt deps into setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+BASE_DIR = Path(__file__).parent
+
+requirements = [
+    line.strip()
+    for line in (BASE_DIR / "requirements.txt").read_text().splitlines()
+    if line.strip() and not line.startswith("#")
+]
 
 setup(
     name="roasterbro",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        'click',
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "roasterbro=roasterbro.main:main",
