4f6379af023958ae92db254ccd288fac0861f1df
Gaohar Imran <gaoharimran29@gmail.com>
2026-08-15 22:12:20 +0530

    fix: handle git repositories with no commits

--- roasterbro/tools/repo_git_scan.py
+++ roasterbro/tools/repo_git_scan.py
@@ -8,18 +8,41 @@ def analyze_git_repository(path: str) -> dict[str, Any]:
     try:
         git_repo = git.Repo(path, search_parent_directories=True)
         git_file_path = git_repo.git_dir
-        commit_count = git_repo.git.rev_list('--count' , 'HEAD')
-        last_commit = git_repo.head.commit.committed_date
-        last_commit_date = datetime.fromtimestamp(last_commit)
-        human_readable_date = last_commit_date.strftime("%B %d, %Y, at %I:%M %p")
-        unique_contributors_count = len(git_repo.git.shortlog("-sn", "HEAD").splitlines())
-        local_branches = git_repo.branches
-        remote_branches = [ref for remote in git_repo.remotes for ref in remote.refs]
-        true_remote_branches = [branch for branch in remote_branches if not branch.name.endswith("/HEAD")]
 
-        today = datetime.now()
-        age_diff = today - last_commit_date
-        days_ago = age_diff.days
+        if git_repo.head.is_valid():
+            commit_count = git_repo.git.rev_list("--count", "HEAD")
+            last_commit = git_repo.head.commit.committed_date
+
+            last_commit_date = datetime.fromtimestamp(last_commit)
+            human_readable_date = last_commit_date.strftime(
+                "%B %d, %Y, at %I:%M %p"
+            )
+
+            unique_contributors_count = len(
+                git_repo.git.shortlog("-sn", "HEAD").splitlines()
+            )
+
+            today = datetime.now()
+            age_diff = today - last_commit_date
+            days_ago = age_diff.days
+        else:
+            commit_count = 0
+            last_commit_date = None
+            human_readable_date = "No commits yet"
+            unique_contributors_count = 0
+            days_ago = None
+
+        local_branches = git_repo.branches
+        remote_branches = [
+            ref
+            for remote in git_repo.remotes
+            for ref in remote.refs
+        ]
+        true_remote_branches = [
+            branch
+            for branch in remote_branches
+            if not branch.name.endswith("/HEAD")
+        ]
 
         return {
             "Git Repository": True,
@@ -31,7 +54,7 @@ def analyze_git_repository(path: str) -> dict[str, Any]:
             "Local Branches": local_branches,
             "Remote Branches": true_remote_branches,
             "No. of local branches": len(local_branches),
-            "No. of remote branches": len(true_remote_branches)
+            "No. of remote branches": len(true_remote_branches),
         }
     
     except git.exc.InvalidGitRepositoryError:
